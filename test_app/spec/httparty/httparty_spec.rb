cbe9e944b2476036cc244c5fca966f99b270a3a6
Matheus Martins <martinsmf00@gmail.com>
2022-06-19 22:15:41 -0300

    vcr

--- test_app/spec/httparty/httparty_spec.rb
+++ test_app/spec/httparty/httparty_spec.rb
@@ -6,14 +6,16 @@ describe "HTTParty" do
   # end
 
   it "content-type webmock" do
-    stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").
-      to_return(
-      status: 200,
-      body: "",
-      headers: { 'content-type': "application/json; charset=utf-8" },
-    )
-    response = HTTParty.get("https://jsonplaceholder.typicode.com/posts/2")
-    content_type = response.headers["content-type"]
-    expect(content_type).to match(/application\/json/)
+    # stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").
+    #   to_return(
+    #   status: 200,
+    #   body: "",
+    #   headers: { 'content-type': "application/json; charset=utf-8" },
+    # )
+    VCR.use_cassette("jsonplaceholder/posts") do
+      response = HTTParty.get("https://jsonplaceholder.typicode.com/posts/2")
+      content_type = response.headers["content-type"]
+      expect(content_type).to match(/application\/json/)
+    end
   end
 end
