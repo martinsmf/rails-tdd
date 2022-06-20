7ec2b744b5c20a38317d8c555b91aa54afe3b6b4
Matheus Martins <martinsmf00@gmail.com>
2022-06-19 22:21:57 -0300

    vcr with rspec's metadatas.

--- test_app/spec/httparty/httparty_spec.rb
+++ test_app/spec/httparty/httparty_spec.rb
@@ -5,17 +5,17 @@ describe "HTTParty" do
   #   expect(content_type).to match(/application\/json/)
   # end
 
-  it "content-type webmock" do
+  it "content-type webmock", vcr: { cassette_name: "jsonplaceholder/posts" } do
     # stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").
     #   to_return(
     #   status: 200,
     #   body: "",
     #   headers: { 'content-type': "application/json; charset=utf-8" },
     # )
-    VCR.use_cassette("jsonplaceholder/posts") do
-      response = HTTParty.get("https://jsonplaceholder.typicode.com/posts/2")
-      content_type = response.headers["content-type"]
-      expect(content_type).to match(/application\/json/)
-    end
+    # VCR.use_cassette("jsonplaceholder/posts") do
+    response = HTTParty.get("https://jsonplaceholder.typicode.com/posts/2")
+    content_type = response.headers["content-type"]
+    expect(content_type).to match(/application\/json/)
   end
+  # end
 end
