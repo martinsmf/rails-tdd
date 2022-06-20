773b3b18bb24373f3f546edd39af0d6ffbcd8f68
Matheus Martins <martinsmf00@gmail.com>
2022-06-19 22:30:43 -0300

    filtering sensitive data

--- test_app/spec/httparty/httparty_spec.rb
+++ test_app/spec/httparty/httparty_spec.rb
@@ -16,6 +16,6 @@ describe "HTTParty" do
     response = HTTParty.get("https://jsonplaceholder.typicode.com/posts/2")
     content_type = response.headers["content-type"]
     expect(content_type).to match(/application\/json/)
+    # end
   end
-  # end
 end
